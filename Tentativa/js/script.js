26502be11d732781d39adb2b3e2f6d21ae8c34c7
Bruno Chaves <brunochavesdeoliveira2003@gmail.com>
2023-07-31 12:26:04 -0300

    4°

--- Tentativa/js/script.js
+++ Tentativa/js/script.js
@@ -1,14 +1,25 @@
 
+
+
 window.addEventListener("scroll", function() {
     var navbar2 = document.querySelector(".navbar2");
-    navbar2.classList.toggle("sticky", window.scrollY > 1350);
+    const pos=navbar2.offsetTop;
+    const scrollPosition=window.scrollY;
+    navbar2.classList.toggle("sticky", window.scrollY>pos);
+    /*if(scrollPosition>pos){
+        navbar2.classList.add("sticky");
+    }else{
+        navbar2.classList.remove("sticky");
+    }*/
+})
+
 
     /*var logo = document.querySelector(".logo1");
 
     if (this.window.scrollY>1350) {
         MudarEstado(logo);
-    }*/
-});
+    }
+});*/
 /*function MudarEstado(el) {
     var display = el.style.display;
     if (display == "none") {
