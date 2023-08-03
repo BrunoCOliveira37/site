bc8bef1f193070a9cbae4d846b15bc21906b9b5e
Bruno Chaves <brunochavesdeoliveira2003@gmail.com>
2023-08-02 21:10:48 -0300

    6°

--- Tentativa2/js/script.js
+++ Tentativa2/js/script.js
@@ -6,3 +6,29 @@ function navbarSticky() {
     navbar2.classList.toggle("sticky", scrollPosition>pos);
 
 }
+
+
+
+var slides=document.querySelectorAll('.slide');
+var btns=document.querySelectorAll('.butn');
+let currentSlide= 1;
+//manual
+var manualNav=function(manual){
+    slides.forEach((slide) => {
+        slide.classList.remove("active");
+
+        btns.forEach((butn)=>  {
+            butn.classList.remove("active");
+        });
+    })
+
+
+    slides[manual].classList.add("active");
+    btns[manual].classList.add("active");
+}
+btns.forEach((butn, i) => {
+    butn.addEventListener("click",()=>{
+        manualNav(i);
+        currentSlide= i;
+    });
+});
